5da751e50f1176cdccd48ca8db2ee2e66b5c5e19
Prabath Peiris <peiris.prabath@gmail.com>
2018-03-07 07:05:11 +0000

    working on the project

--- steeringNN_nvidia.py
+++ steeringNN_nvidia.py
@@ -1,34 +1,34 @@
 import csv
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 import cv2
-from keras.models import Sequential 
+from keras.models import Sequential
 from keras.layers import Flatten, Dense, Lambda, Cropping2D
 from keras.layers.convolutional import Convolution2D, MaxPooling2D
- 
-# load the data from csv file 
+
+# load the data from csv file
 
 data = pd.read_csv(
 	"data/set_2_moredata_steering/data/driving_log.csv",
 	names=['image_center', 'image_left', 'image_right', 'steering', 'throttle', 'brake', 'speed']
 );
 
-# correct the image path 
-# load the image path to an array 
+# correct the image path
+# load the image path to an array
 data['image_center']=data['image_center'].apply(lambda x: 'data/set_2_moredata_steering/data/IMG/' + x.split('/')[-1])
 data['image_left']=data['image_left'].apply(lambda x: 'data/set_2_moredata_steering/data/IMG/' + x.split('/')[-1])
 data['image_right']=data['image_right'].apply(lambda x: 'data/set_2_moredata_steering/data/IMG/' + x.split('/')[-1])
 
-# load steering data to an array 
+# load steering data to an array
 # data is in the 'steeting'
 
 # load images
-# getting the center image 
+# getting the center image
 images_center_1 = data['image_center'].apply(lambda x: cv2.imread(x))
 # flip the center image
 images_center_2 = data['image_center'].apply(lambda x: np.fliplr(cv2.imread(x)))
 
-# left images 
+# left images
 images_left_1 = data['image_left'].apply(lambda x: cv2.imread(x))
 
 # right images
@@ -44,12 +44,12 @@ lables = list(data['steering']) + list(-data['steering']) + list(lables_left) + 
 
 # print(lables)
 
-# convert data to numpy arrays 
+# convert data to numpy arrays
 x_train = np.array(images)
 y_train = np.array(lables)
 # print(x_train[0].shape)
 
-# create a model 
+# create a model
 # print(x_train.shape)
 # print(y_train.shape)
 
@@ -71,19 +71,19 @@ model.add(Dense(1))
 
 model.compile(loss='mse', optimizer='adam')
 model.fit(
-	x_train, 
-	y_train, 
-	nb_epoch=3,	
-	validation_split=0.2, 
+	x_train,
+	y_train,
+	nb_epoch=3,
+	validation_split=0.2,
 	shuffle=True,
-	verbose=2
+	verbose=1
 )
 
 model.save('model_steering_nvidia.h5')
 
 
 
-# train the model 
+# train the model
 
 # save the model
 
