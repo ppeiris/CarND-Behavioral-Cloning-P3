a8ee5aa09b6d618d26a4a09fb173957141edd204
Prabath Peiris <peiris.prabath@gmail.com>
2018-03-08 07:30:18 +0000

    add model.py

--- model.py
+++ model.py
@@ -18,35 +18,20 @@ from sklearn.model_selection import train_test_split
 import sklearn
 
 # Configs
-# dloc = [
-#         # "data/set_1_steering/data",
-#         # "data/set_2_moredata_steering/data",
-#         # "data/data_track1_center_counter_drive/data",
-#         "data/data_track1_center_drive/data",
-#         "data/track1_center",
-#         "data/track1_smooth_corners",
-#         "data/track1_recovery_from_sides",
-#         "data/track1_counterclock",
-#         "data/track2_center",
-#         "data/track1_more_corners",
-#         "data/track1_counter_corners"
-#     ]
-
-
-
-
 dloc = [
     "newdata/track1_center",
     "newdata/track1_corners",
     "newdata/track1_counter_center",
     "newdata/track1_counter_corners",
     "newdata/track1_recovery_corners",
+    # "data/data_samples",
     "data/track2_center",
     "data/track1_center",
     "data/track1_smooth_corners",
     "data/track1_recovery_from_sides",
     # "data/track1_counterclock",
     "data/track2_center",
+    "newdata/track1_center_2",
     # "data/track1_more_corners",
     # "data/track1_counter_corners"
 ];
@@ -88,17 +73,22 @@ def loadData():
     totalData = shuffle(totalData)
     # make some adjustment to the steeting
     totalData = steeringAdjustments(totalData)
-    # print(data.tail())
-    # print(len(totalData))
-    # print('-----------------------')
+
     return totalData
 
+"""
 
+split the data in to training and validation sets
+"""
 def getSamples():
     data = loadData()
     train_samples, validation_samples = train_test_split(data, test_size=0.2)
     return [train_samples, validation_samples]
 
+"""
+
+generator that return the image data as an numpy array
+"""
 def generator(samples, batch_size=32):
     num_samples = len(samples)
 
@@ -146,7 +136,7 @@ def getNvideaModel():
     # normalize images
     model.add(Lambda(lambda x: (x/255.0) - 0.5, input_shape=(160, 320, 3)))
     model.add(Cropping2D(cropping=((70, 25),(0,0))))
-    model.add(BatchNormalization())
+    # model.add(BatchNormalization())
     model.add(Convolution2D(24, 5, 5, subsample=(2, 2), activation="relu"))
     # model.add(BatchNormalization())
     model.add(Convolution2D(36, 5, 5, subsample=(2, 2), activation="relu"))
@@ -158,11 +148,8 @@ def getNvideaModel():
     model.add(Convolution2D(64, 3, 3, activation="relu"))
     # model.add(BatchNormalization())
     model.add(Flatten())
-    model.add(Dropout(0.7))
     model.add(Dense(100))
-    model.add(Dropout(0.7))
     model.add(Dense(50))
-    model.add(Dropout(0.7))
     model.add(Dense(10))
     model.add(Dense(1))
     model.compile(loss='mse', optimizer='adam')
@@ -181,17 +168,16 @@ def main():
         samples_per_epoch=len(train_samples),
         validation_data=validation_generator,
         nb_val_samples=len(validation_samples),
-        nb_epoch=5
+        nb_epoch=3
     )
 
-    model.save('model_1.h5')
+    model.save('model1.h5')
 
-
-    print(history_object.history.keys())
-    print('Loss')
-    print(history_object.history['loss'])
-    print('Validation Loss')
-    print(history_object.history['val_loss'])
+    # print(history_object.history.keys())
+    # print('Loss')
+    # print(history_object.history['loss'])
+    # print('Validation Loss')
+    # print(history_object.history['val_loss'])
 
 
 if __name__ == '__main__':
